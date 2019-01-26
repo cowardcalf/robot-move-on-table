ca9571b97e2f29d4074cbfb9863a1387d076e3a9
Yang Liu <cowardcalf@gmail.com>
2019-01-26 15:17:10 +1100

    Init IO dependencies

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
-function hello() {
-  console.log('Hello World!');
+import chalk from 'chalk';
+import figlet from 'figlet';
+import inquirer from 'inquirer';
+
+const askQuestions = () => {
+  const questions = [
+    {
+      name: "commands",
+      type: "input",
+      message: "Please input the commands (separated by space):"
+    }
+  ];
+  return inquirer.prompt(questions);
+};
+
+const init = () => {
+  console.log(
+    chalk.green(
+      figlet.textSync("Robot Move on Table", {
+        font: "standard",
+        horizontalLayout: "default",
+        verticalLayout: "default"
+      })
+    )
+  );
 }
 
-hello();
+const run = async () => {
+  // show script introduction
+  init();
+
+  // ask questions
+  const answers = await askQuestions();
+  const { commands } = answers;
+  console.log(
+    chalk.blueBright(`Commands: ${commands}`)
+  )
+};
+
+run();
