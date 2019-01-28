9f3b91f6bdcf6ad389e11556a550dca89f487dd0
Yang Liu <cowardcalf@gmail.com>
2019-01-28 16:47:16 +1100

    Add comments for logger

--- utils/logger.js
+++ utils/logger.js
@@ -1,11 +1,24 @@
+/**
+ * Console output utilities.
+ * Using chalk.
+ */
+
 import chalk from 'chalk';
 
+/**
+ * Standard output, colour bright blue
+ * @param {string} message
+ */
 const logStd = (message) => {
   console.log(
     chalk.blueBright(message)
   );
 }
 
+/**
+ * Simply console log out.
+ * @param {string} message
+ */
 const logDebug = (message) => {
   console.log(message);
 }
